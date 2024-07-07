03502a737d1603e46b1e4f7ba48f29a4afba5e4a
Hexer611 <32894909+Hexer611@users.noreply.github.com>
2024-07-08 00:23:54 +0300

    Initial commit

---
+++ Source/WinMain.cpp
@@ -0,0 +1,70 @@
+#include <windows.h>
+
+#define MAX_NAME_STRING 256
+#define HInstance() GetModuleHandle(NULL)
+
+WCHAR WindowClass[MAX_NAME_STRING];
+WCHAR WindowTitle[MAX_NAME_STRING];
+
+INT WindowWidth;
+INT WindowHeight;
+
+int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
+{
+	// Initialize global variables
+
+	wcscpy_s(WindowTitle, TEXT("Erdem Title Windwo"));
+	wcscpy_s(WindowClass, TEXT("ErdemClass"));
+
+	WindowWidth = 1366;
+	WindowHeight = 768;
+
+	// Create window class
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
+
+	wcex.hIcon = LoadIcon(0, IDI_APPLICATION);
+	wcex.hIconSm = LoadIcon(0, IDI_APPLICATION);
+
+	wcex.lpszClassName = WindowClass;
+	wcex.lpszMenuName = nullptr;
+
+	wcex.hInstance = HInstance();
+
+	wcex.lpfnWndProc = DefWindowProc;
+
+	RegisterClassEx(&wcex);
+
+	// Show the window
+	
+	HWND hWnd = CreateWindow(WindowClass, WindowTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, WindowWidth, WindowHeight, nullptr, nullptr, HINSTANCE(), nullptr);
+
+	if (!hWnd)
+	{
+		MessageBox(0, L"Fail Man", 0, 0);
+		return 0;
+	}
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	// Listen for message events
+
+	MSG msg = { 0 };
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return 0;
+}
